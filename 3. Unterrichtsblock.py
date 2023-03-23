c77e8212c71f611077a0ee300b4dd57bbf06220f
Danjka99 <jakob_daniel@gmx.net>
2023-03-23 15:42:20 +0100

    sync

---
+++ 3. Unterrichtsblock.py
@@ -0,0 +1,5 @@
+anzahl = 3
+produktnummer = 567
+preis = 49.95
+meinebestellung = "Ich bezahle {2} EUR für {0} Stück von Produktnummer {1}."
+print(meinebestellung.split("EUR"))
---
+++ 4. Unterrichtsblock.py
@@ -0,0 +1,8 @@
+x = 5
+print(x > 3 and x < 10)  # Gibt True aus, weil 5 größer als 3 AND 5 kleiner als 10 ist
+
+x = 5
+print(x > 3 or x < 4)  # Gibt True aus, weil eine der Bedingungen wahr ist 
+
+x = 5
+print(not(x > 3 and x < 10))  # Gibt False aus, weil beide Bedingungen wahr sind
