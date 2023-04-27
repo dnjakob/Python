2a61b88bf642021b441e2dc8ac13134fe285b307
Danjka99 <jakob_daniel@gmx.net>
2023-04-27 14:43:56 +0200

    sync

---
+++ Listen.py
@@ -0,0 +1,19 @@
+# Menge als Set
+feature = {4, 35.6, 34.2, "Beige", "Hund"}
+
+# Menge als Tupel
+feature = (4, 35.6, 34.2, "Beige", "Hund")
+
+# Menge als Liste
+feature = [4, 35.6, 34.2, "Beige", "Hund"]
+
+# Menge als Dictionary
+feature = {
+ 'beine': 4,
+ 'groesse': 35.6,
+ 'breite': 32.2,
+ 'farbe': "Braun",
+ 'label': "Hund"
+}
+
+print(feature["groesse"])
