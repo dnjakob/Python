9eaf64399b752cc797198d98b0cbb028d797d787
Danjka99 <jakob_daniel@gmx.net>
2023-05-19 17:58:50 +0200

    sync

--- Künstliche Intelligenz 2.py
+++ Künstliche Intelligenz 2.py
@@ -3,14 +3,12 @@ import numpy as np
 beine = [4, 4, 4, 4, 2]
 groesse = [35.6, 28.4, 55.9, 46.1, 5.2]
 breite = [32.2, 18.5, 42.8, 39.3, 4.1]
-farbe = ["Braun", "Schwarz", "Beige", "Weiß", "Rot"]
-label = ["Hund", "Hund", "Hund", "Hund", "nicht Hund"]
+label = [1, 1, 1, 1, 0]
 
 rohdaten = {
  'beine': beine,
  'groesse': groesse,
  'breite': breite,
- 'farbe': farbe,
  'label': label
 }
 
@@ -19,4 +17,10 @@ beine = np.array(rohdaten['beine'])
 
 feature = np.array([rohdaten['beine'], rohdaten['groesse'], rohdaten['breite']])
 
-print(feature.shape)
+print("Durchschnittswert der Beine: ",np.mean(rohdaten['beine']))
+
+print("Durchschnittswert der Groesse: ",np.mean(rohdaten['groesse']))
+
+print("Durchschnittswert der Breite: ",np.mean(rohdaten['beine']))
+
+print("Durchschnittswert der Label: ",np.mean(rohdaten['label']))
