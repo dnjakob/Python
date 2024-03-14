254233887a4a528a98dfae81366aa8c1e48bca85
dnjakob <126343846+dnjakob@users.noreply.github.com>
2024-03-14 08:11:24 +0100

    sync

---
+++ 7_unterrichtsblock_info2.py
@@ -0,0 +1,97 @@
+import numpy
+from scipy import stats
+ 
+##  Maschinelles Lernen
+#   Aufgabe 1
+
+zahlen_aufg01 = [8, 2, 5, 1, 10]
+
+def berechene_mittelWert(liste):
+    mittelWert = numpy.mean(liste)
+    return mittelWert
+ 
+berechene_mittelWert(zahlen_aufg01)
+ 
+#   Aufgabe 2
+
+zahlen1_aufg02 = [7, 5, 3, 5]
+zahlen2_aufg02 = [1, 2, 3, 4, 5]
+
+def berechne_median(liste):
+    median = numpy.median(liste)
+    return(median)
+ 
+berechne_median(zahlen1_aufg02)
+berechne_median(zahlen2_aufg02)
+ 
+#   Aufgabe 3
+
+zahlen_aufg03 = [4, 1, 2, 2, 3, 1]
+
+def berechne_modus(liste):
+    modus = stats.mode(liste)
+    return(modus)
+ 
+berechne_modus(zahlen_aufg03)
+ 
+#   Aufgabe 4
+zahlen_aufg04 = [1, 2, 3, 4, 4, 5, 5, 5]
+
+def kombinierte_statistik(liste):
+    mittelWert = numpy.mean(liste)
+    median = numpy.median(liste)
+    modus = stats.mode(liste)
+    tuple = (mittelWert, median, modus)
+    return tuple
+ 
+kombinierte_statistik(zahlen_aufg04)
+ 
+#   Aufgabe 5
+
+zahlen_aufg05 = [10, 20, 30, 40, 50, 60, 70]
+
+def filter_und_statistik(liste:list, minWert, maxWert):
+    for zahl in liste:
+        if zahl < minWert or zahl > maxWert:
+            liste = liste.remove(zahl)
+    mittelWert = numpy.mean(liste)
+    median = numpy.median(liste)
+    modus = stats.mode(liste)
+    tuple = (mittelWert, median, modus)
+    return tuple
+ 
+filter_und_statistik(zahlen, 20, 60)
+ 
+##  Standardabweichung
+#   Aufgabe 1
+
+zahlen_aufg06 = [4, 8, 6, 5, 3, 2]
+
+def berechne_varianz(liste):
+    mittelWert = numpy.mean(liste)
+    differenz = [(wert - mittelWert) **2 for wert in liste]
+    varianz = numpy.mean(differenz)
+    return varianz
+       
+berechne_varianz(zahlen_aufg06)
+ 
+#   Aufgabe 2
+
+zahlen_aufg07 = [9, 2, 5, 4, 12, 7, 8, 11]
+
+def berechene_standardabweichung(liste):
+    standardabweichung = numpy.sqrt(berechne_varianz(liste))
+    return(standardabweichung)
+ 
+berechene_standardabweichung(zahlen_aufg07)
+
+ 
+#   Berechnung von Perzentile
+
+schulstrecke = [20, 30, 5, 10, 40, 20]
+
+def percentile(liste):
+     percentile = numpy.percentile(liste, 75)
+     print(percentile)
+ 
+percentile(schulstrecke)
